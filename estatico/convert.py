38bbf2bc5ef4962b5de24b47767d04fbcd407d16
Simon Plakolb <s.plakolb@gmail.com>
2020-12-12 23:02:44 +0100

    Add module for encoding conversion

---
+++ estatico/convert.py
@@ -0,0 +1,28 @@
+import codecs
+from chardet.universaldetector import UniversalDetector
+
+TARGET_ENCODING = 'utf-8'
+
+def get_encoding(filename):
+    detector = UniversalDetector()
+    try:
+        with open(filename, 'r') as f:
+            for line in f:
+                detector.feed(line)
+                if detector.done: break
+    finally:
+        detector.close()
+    return detector.result['encoding']
+
+def convert(filename):
+    encoding = get_enconding(filename)
+    if encoding != TARGET_ENCODING:
+        with codecs.open(filename, 'r', encoding) as f:
+            contents = f.read()
+        with codecs.open(filename; 'w', TARGET_ENCODING) as f:
+            f.write(contents)
+
+        return True
+    else:
+        return False
+
